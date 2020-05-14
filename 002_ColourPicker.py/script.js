bd6aa20ca66210becf0918c7143a6eb35d80736f
Arun Arjunan <arun.arjunan1@outlook.com>
2020-05-15 00:03:20 +0800

    final updates, overhaul

--- 002_ColourPicker.py/script.js
+++ 002_ColourPicker.py/script.js
@@ -1,16 +1,16 @@
-document.getElementById('colour_mix').innerHTML = "hello"
-
 function changeColour() {
-    let display = document.getElementById("display");
-    let red = document.getElementById('red').value;
-    let green = document.getElementById('green').value;
-    let blue = document.getElementById('blue').value;
-    let colour = 'rgb(' + red + ',' + green + ',' + blue + ')';
-    display.style.backgroundColor = colour
-    document.getElementById("colour_mix").innerHTML = "#" + colour;
+    let red = Number(document.getElementById('red').value).toString(16);
+    let green = Number(document.getElementById('green').value).toString(16);
+    let blue = Number(document.getElementById('blue').value).toString(16);
+    let colour = "#" + red + green + blue;
+    document.getElementById("colour_mix").innerHTML = colour;
+    document.getElementById("display").style.background = colour;
+    document.getElementById("colour_mix").style.color = colour;
 
 }
-
-document.getElementById("red").addEventListener('click', changeColour);
-document.getElementById("blue").addEventListener('input', changeColour);
-document.getElementById("green").addEventListener('input', changeColour);
+document.getElementById("red").addEventListener('touchmove', changeColour);
+document.getElementById("blue").addEventListener('touchmove', changeColour);
+document.getElementById("green").addEventListener('touchmove', changeColour);
+document.getElementById("red").addEventListener('mousemove', changeColour);
+document.getElementById("blue").addEventListener('mousemove', changeColour);
+document.getElementById("green").addEventListener('mousemove', changeColour);
